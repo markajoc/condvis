0ebbd047313e5d1142dfa3b19a07b9a0eec37da7
markajoc <mark_ajoc@yahoo.ie>
2015-10-21 18:10:20 +0100

    apply as.matrix() to X at start

--- R/mydist.R
+++ R/mydist.R
@@ -1,7 +1,8 @@
 mydist <- 
 function (x, X, p = 2, inf = FALSE)
 {
-    dif <- abs(as.matrix(X) - matrix(as.numeric(x), nrow = nrow(X), ncol = length(x), byrow = TRUE))
+    X <- as.matrix(X)
+    dif <- abs(X - matrix(as.numeric(x), nrow = nrow(X), ncol = length(x), byrow = TRUE))
     if (inf)
         return(apply(dif, 1, max))
     tmp <- dif^p
