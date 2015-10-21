23e69d8a0b393c45d1b4af27704c057809bc275d
markajoc <mark_ajoc@yahoo.ie>
2015-10-21 17:03:18 +0100

    Remove conditions for type of X
    mydist throws an error when nrow(X) misfires due to an inappropriate type of X (when X has only one column, for example). Removing an unsuccessful attempt to fix this.

--- R/mydist.R
+++ R/mydist.R
@@ -1,9 +1,6 @@
 mydist <- 
 function (x, X, p = 2, inf = FALSE)
 {
-    n <- if (is.vector(X))
-        length(X)
-    else nrow(X)    
     dif <- abs(as.matrix(X) - matrix(as.numeric(x), nrow = nrow(X), ncol = length(x), byrow = TRUE))
     if (inf)
         return(apply(dif, 1, max))
