efc362dbb17a5d96d3a61afbdd2ea4665e2e6753
Mark O'Connell <mark_ajoc@yahoo.ie>
2015-10-28 17:23:52 +0000

    fix bug in mydist().. again

--- R/mydist.R
+++ R/mydist.R
@@ -2,9 +2,9 @@ mydist <-
 function (x, X, p = 2, inf = FALSE)
 {
     X <- if (is.null(dim(X)))
-        matrix(X, ncol = length(X))
+        matrix(X, ncol = length(x))
     else as.matrix(X)
-    dif <- abs(X - matrix(as.numeric(x), nrow = nrow(X), ncol = ncol(X), byrow = TRUE))
+    dif <- abs(X - matrix(as.numeric(x), nrow = nrow(X), ncol = ncol(x), byrow = TRUE))
     if (inf)
         return(apply(dif, 1, max))
     tmp <- dif^p
