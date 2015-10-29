ead0d3a29bdaed1b841126084d48f83cf7a82b6f
Mark O'Connell <mark_ajoc@yahoo.ie>
2015-10-29 13:13:33 +0000

    allow saving current ceplot() to pdf

--- R/keystroke.ceplot.R
+++ R/keystroke.ceplot.R
@@ -22,10 +22,34 @@ function (key)
     }
     if (identical(key, "s")){
         plotobject <- get("plotobject", envir = parent.frame())
-        filename <- paste("snapshot_", gsub(":", ".", gsub(" ", "_", Sys.time())), sep = "")
-        pdf(file = filename)
-        
+        lenC <- length(plotobject$xcplots)
+        Xc.cond <- plotobject$xsplot$xc.cond
+        n.selector.cols <- ceiling(lenC / 4L)
+        select.colwidth <- max(min(0.18 * n.selector.cols, 0.45), 0.2)
+        select.lwd <- plotobject$xcplots[[1]]$select.lwd
+        select.col <- plotobject$xcplots[[1]]$select.col
+        sigma <- if (is.null(plotobject$sigma))
+            1
+        else plotobject$sigma 
+        filename <- paste("snapshot_", gsub(":", ".", gsub(" ", "_", Sys.time())), ".pdf", sep = "") 
+        #width <- 10 + 2 * n.selector.cols  
+        width <- dev.size()[1]  
+        height <- dev.size()[2]         
+        pdf(file = filename, width = width, height = height)
+        close.screen(all.screens = T)
+        main <- split.screen(figs = matrix(c(0, 1 - (select.colwidth), 1 - (select.colwidth), 1, 0, 0, 1, 1), ncol = 4))
+        selectors <- split.screen(figs = c(max(ceiling(lenC / n.selector.cols), 3), n.selector.cols),
+        screen = main[2])   
+        screen(main[1])
+        do.call(plotxs, plotobject$xsplot)
+        for (i in 1:lenC){
+            screen(selectors[i])
+            plotxc(xc = plotobject$xcplots[[i]]$xc, xc.cond = Xc.cond[1, plotobject$xcplots[[i]]$name], 
+                select.lwd = select.lwd, select.col = select.col, name = plotobject$xcplots[[i]]$name)
+        }
         dev.off()
+        cat(paste("\nSnapshot saved: '", filename,"'", sep = ""))
+        cat(paste("\ndistance: ", plotobject$type, ", sigma: ", sigma, sep = ""))
     }
     points(NULL)
 }
