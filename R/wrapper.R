6543e018a9e49443fe46e16e1386e7ac4d654f14
Mark O'Connell <mark_ajoc@yahoo.ie>
2015-10-29 15:35:02 +0000

    write wrapper function

--- R/wrapper.R
+++ R/wrapper.R
@@ -1,7 +1,23 @@
 wrapper <- 
-function (data, model, response = NULL, S = NULL, C = NULL, sigma = NULL, 
-    threshold = NULL, type = "euclidean", cex.axis = NULL, cex.lab = NULL, tck = NULL, 
-    view3d = FALSE, method = "default")
+function (data, model, type = "default", response = NULL, S = NULL, C = NULL, 
+    sigma = NULL, distance = "euclidean", cex.axis = NULL, cex.lab = NULL, 
+    tck = NULL, view3d = FALSE, Corder = "default", selectortype = "minimal", 
+    width = 9, height = 7)
 {
-
+    if (identical(type, "default")){
+        interactiveceplot(data = data, model = model, response = response, 
+            S = S, C = C, sigma = sigma, threshold = NULL, type = distance, 
+            cex.axis = cex.axis, cex.lab = cex.lab, tck = tck, view3d = view3d, 
+            method = Corder, width = width, height = height)
+    } else if (identical(type, "separate")){
+        separate(data = data, model = model, response = response, S = S, C = C, sigma = sigma, 
+            threshold = NULL, type = distance, cex.axis = cex.axis, cex.lab = 
+            cex.lab, tck = tck, view3d = view3d, method = Corder, selectortype =
+            selectortype)
+    } else if (identical(type, "shiny")){
+        shinyceplot(data = data, model = model, response = response, S = S, 
+            C = C, sigma = sigma, threshold = NULL, type = distance, 
+            cex.axis = cex.axis, cex.lab = cex.lab, tck = tck, view3d = 
+            view3d, method = Corder, selectortype = "minimal")
+    }
 }
