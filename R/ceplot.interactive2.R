5c723b0030c070b54ad1f259023c12345eab49a0
Mark O'Connell <mark_ajoc@yahoo.ie>
2015-12-01 14:24:30 +0000

    add new function for ceplot.interactive

---
+++ R/ceplot.interactive2.R
@@ -0,0 +1,150 @@
+ceplot.interactive2 <- 
+function (data, model, response = NULL, S = NULL, C = NULL, sigma = NULL, 
+    distance = "euclidean", cex.axis = NULL, cex.lab = NULL, tck = NULL, 
+    view3d = FALSE, Corder = "default")
+{
+    data <- na.omit(data)
+    model <- if (!identical(class(model), "list"))
+        list(model)
+    else model
+    model.name <- if (!is.null(names(model)))
+        names(model)
+    else NULL
+    varnamestry <- try(getvarnames(model[[1]]), silent = TRUE)
+    response <- if (is.null(response))
+        if (class(varnamestry) != "try-error")
+           which(colnames(data) == varnamestry$response[1])
+        else stop("could not extract response from 'model'.")
+    else if (is.character(response))
+            which(colnames(data) == response)
+        else response
+    S <- if(is.null(S)){
+         (1:ncol(data))[-response][1L]
+        #cat(paste("\n'S' was not specified, picked", colnames(data)[S]))
+        } else if (is.character(S))
+            vapply(S, function(x) which(colnames(data) == x), numeric(1))
+            else S
+    C <- if (is.null(C))
+        if (class(varnamestry) != "try-error"){
+            possibleC <- unique(unlist(lapply(
+                lapply(model, getvarnames), `[[`, 2)))
+            arrangeC(data[, possibleC[!(possibleC %in% colnames(data)[S])], 
+                drop = FALSE], method = Corder)
+        } else arrangeC(data[, -c(response, S)])
+    else C
+    C <- if (all(vapply(C, is.numeric, logical(1))))
+        as.list(C)
+    else if (all(vapply(C, is.character, logical(1))))
+            lapply(C, match, table = colnames(data))
+        else
+            stop("'C' should be a vector or list (containing vectors of length",
+                 " 1 or 2) with integer column indices or character variable",
+                 " names from 'data'.")
+    uniqC <- unique(unlist(C))
+    if (any(response %in% uniqC))
+        stop("cannot have 'response' variable in 'C'")
+    if (any(response %in% S))
+        stop("cannot have 'response' variable in 'S'")
+    #if (!identical(length(unique(vapply(lapply(model, getvarnames), 
+    #    `[[`, character(1), 1))), 1L))
+    #    stop("cannot compare models with different response variables") 
+    if (!identical(length(intersect(S, uniqC)), 0L))
+        stop("cannot have variables common to both 'S' and 'C'")    
+    xc.cond <- data[1, uniqC, drop = FALSE]
+ 
+    xcplots <- list()
+    coords <- matrix(ncol = 4, nrow = length(C))    
+    plotlegend <- length(S) == 2
+    n.selector.cols <- ceiling(length(C) / 4L)
+    height <- 7
+    width <- height + 0.5 * plotlegend + 2.2 * n.selector.cols
+    
+    if (identical(version$os, "linux-gnu"))
+        x11(type = "Xlib", height = height, width = width)
+    else
+        x11(height = height, width = width)
+    close.screen(all.screens = TRUE)
+    mainscreens <- split.screen(figs = matrix(c(0, 1 - 0.2, 1 - 0.2, 1, 
+        0, 0, 1, 1), ncol = 4))
+    xcscreens <- split.screen(c(4, 1), screen = mainscreens[2])
+    for (i in seq_along(C)){
+        screen(xcscreens[i])
+        xcplots[[i]] <- plotxc(xc = data[, C[[i]]], xc.cond = data[1, C[[i]]], 
+            name = colnames(data[, C[[i]], drop = FALSE]), select.col = "blue")
+        coords[i, ] <- par("fig")
+    }    
+    legendwidth <- 0.15
+    xsscreens <- if (plotlegend){
+        split.screen(figs = matrix(c(0, 1 - legendwidth, 1 - legendwidth, 1, 
+            0, 0, 1, 1), ncol = 4), screen = mainscreens[1])
+    } else mainscreens[1]
+    if (plotlegend){
+        screen(xsscreens[2])
+        xslegend(data[, response], colnames(data)[response])
+    }
+    screen(xsscreens[1])
+    vw <- visualweight(xc = data[, uniqC, drop = FALSE], xc.cond = xc.cond)
+    xsplot <- plotxs.shiny(xs = data[, S, drop = FALSE], data[, response, drop = FALSE], 
+        xc.cond = xc.cond, model = model, data.colour = rgb(1 - vw$k, 1 - vw$k, 1 - vw$k)[vw$order], data.order = 
+        vw$order, view3d = view3d, theta3d = 45, phi3d = 20)
+    xscoords <- par("fig")
+    
+    mouseclick <- function ()
+    {
+        function (buttons, x, y)
+        {
+            plotindex <- which(apply(coords, 1, `%inrectangle%`, point = c(x, y)))
+            if (length(plotindex) > 0 && if(exists("buttons")) 0 %in% buttons){
+                dev.hold()
+                xcplots[[plotindex]] <<- update(xcplots[[plotindex]], x, y)
+                xc.cond[, xcplots[[plotindex]]$name] <<- xcplots[[plotindex]]$xc.cond.old
+                par(bg = "white")
+                screen(xsscreens[1], new = TRUE)
+                vw <- visualweight(xc = data[, uniqC, drop = FALSE], xc.cond = xc.cond)
+                xsplot <<- plotxs.shiny(xs = xsplot$xs, y = xsplot$y, xc.cond = xc.cond, model = model, model.colour =
+                    xsplot$model.colour, model.lwd = xsplot$model.lwd, model.lty = xsplot$model.lty, model.name = xsplot$model.name, 
+                    yhat = xsplot$yhat, mar = xsplot$mar, data.colour = rgb(1 - vw$k, 1 - vw$k, 
+                    1 - vw$k)[vw$order], data.order = vw$order, view3d = xsplot$view3d, 
+                    theta3d = xsplot$theta3d, phi3d = xsplot$phi3d)
+                dev.flush()
+            }
+            if (findInterval(x, xscoords[1:2]) == 1){
+                if(!(0 %in% buttons)){
+                xstart <<- x
+                ystart <<- y
+                }
+                if(0 %in% buttons){
+                    par(bg = "white")
+                    screen(xsscreens[1], new = TRUE)
+                    xsplot <<- plotxs.shiny(xs = xsplot$xs, y = xsplot$y, xc.cond = xc.cond, model = model, model.colour =
+                        xsplot$model.colour, model.lwd = xsplot$model.lwd, model.lty = xsplot$model.lty, model.name = xsplot$model.name, 
+                        yhat = xsplot$yhat, mar = xsplot$mar, data.colour = rgb(1 - vw$k, 1 - vw$k, 
+                        1 - vw$k)[vw$order], data.order = vw$order, view3d = xsplot$view3d, 
+                        theta3d = xsplot$theta3d + 8 * (xstart - x), phi3d = xsplot$phi3d + 8 * (ystart - y))                
+                }
+            }
+        points(NULL)
+        }
+    }    
+    keystroke <- function ()
+    {
+        function (key)
+        {
+            if (view3d && any(key %in% c("Up", "Down", "Left", "Right"))){
+                par(bg = "white")
+                screen(xsscreens[1], new = TRUE)
+                xsplot <<- plotxs.shiny(xs = xsplot$xs, y = xsplot$y, xc.cond = xc.cond, model = model, model.colour =
+                    xsplot$model.colour, model.lwd = xsplot$model.lwd, model.lty = xsplot$model.lty, model.name = xsplot$model.name, 
+                    yhat = xsplot$yhat, mar = xsplot$mar, data.colour = rgb(1 - vw$k, 1 - vw$k, 
+                    1 - vw$k)[vw$order], data.order = vw$order, view3d = xsplot$view3d, 
+                    theta3d = xsplot$theta3d - 2 * (key == "Right") + 2 * (key == "Left"), phi3d = xsplot$phi3d - 2 * (key == "Up") + 2 * (key == "Down"))
+                dev.flush()
+            }
+        points(NULL)
+        }
+    }      
+    setGraphicsEventHandlers(onMouseDown = mouseclick(), onMouseMove = 
+        mouseclick(), onKeybd = keystroke())
+    getGraphicsEventEnv()
+    getGraphicsEvent()
+}
