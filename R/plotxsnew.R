3600a90ec14dae1c7c077492eac3b5af98bbbfe7
Mark O'Connell <mark_ajoc@yahoo.ie>
2015-12-07 09:11:49 +0000

    new file for plotxs function

---
+++ R/plotxsnew.R
@@ -0,0 +1,7 @@
+plotxs1 <- 
+function (xs, y, xc.cond, model, model.colour = NULL, model.lwd = NULL, 
+    model.lty = NULL, model.name = NULL, yhat = NULL, mar = NULL, data.colour = 
+    NULL, data.order = NULL, view3d = FALSE, theta3d = 45, phi3d = 20)
+{
+
+}
