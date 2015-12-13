e24bb744cf5e77cc0152fd7644f9967ee1735a30
Mark O'Connell <mark_ajoc@yahoo.ie>
2015-12-13 12:56:20 +0000

    add rule for rpart

--- R/predict1.R
+++ R/predict1.R
@@ -4,6 +4,8 @@ function (object, ...)
 {
     type <- if ("nnet" %in% class(object))
         "class"
+    else if ("rpart" %in% class(object)) 
+        "vector"
     else "response"    
     predict(object, ..., n.trees = object$n.trees, type = type)
 }
