0baeefd2afeaab65e872c0f48e80211e77f82fb5
Mark O'Connell <mark_ajoc@yahoo.ie>
2015-12-14 10:11:27 +0000

    tidy line lengths

--- R/barplot2.R
+++ R/barplot2.R
@@ -2,8 +2,8 @@ barplot2 <- function (height, width = 1, space = NULL, names.arg = NULL,
     legend.text = NULL, beside = FALSE, horiz = FALSE, density = NULL, 
     angle = 45, col = NULL, border = par("fg"), main = NULL, 
     sub = NULL, xlab = NULL, ylab = NULL, xlim = NULL, ylim = NULL, 
-    xpd = TRUE, log = "", axes = TRUE, axisnames = TRUE, cex.axis = par("cex.axis"), 
-    cex.names = par("cex.axis"), inside = TRUE, plot = TRUE, 
+    xpd = TRUE, log = "", axes = TRUE, axisnames = TRUE, cex.axis = 
+    par("cex.axis"), cex.names = par("cex.axis"), inside = TRUE, plot = TRUE, 
     axis.lty = 0, offset = 0, add = FALSE, args.legend = NULL, 
     ...) 
 {
