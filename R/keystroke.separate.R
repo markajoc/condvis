491041afc7d019760b0402cda3f5d12b878b0d58
Mark O'Connell <mark_ajoc@yahoo.ie>
2015-12-14 10:01:17 +0000

    tidy line lengths

--- R/keystroke.separate.R
+++ R/keystroke.separate.R
@@ -1,30 +1,32 @@
 .keystroke.separate <-
 function (key)
 {
-    if (identical(key, "q")) return(invisible(1))
+    if (identical(key, "q")) 
+        return(invisible(1))
     expectationwindow <- get("expectationwindow", envir = parent.frame())
     selectorwindow <- get("selectorwindow", envir = parent.frame())
-    if (any(vapply(c("Up", "Down", "Left", "Right"), identical, logical(1), key))){
-    plotxsobject <- get("plotxsobject", envir = parent.frame())
-    Xc <- get("Xc", envir = parent.frame())
-    Xc.cond <- plotxsobject$xc.cond
-    if (plotxsobject$view3d){
-        dev.hold()
-            try(plotxsobject$xc.cond <- get("Xc.cond", envir = parent.frame()), silent = TRUE)
-            if (identical(key, "Down"))
-                plotxsobject$phi3d <- plotxsobject$phi3d + 2
-            if (identical(key, "Up"))
-                plotxsobject$phi3d <- plotxsobject$phi3d - 2
-            if (identical(key, "Left"))
-                plotxsobject$theta3d <- plotxsobject$theta3d + 2
-            if (identical(key, "Right"))
-                plotxsobject$theta3d <- plotxsobject$theta3d - 2
-            dev.set(expectationwindow)
-            par(bg = "white")
-            close.screen(all.screens = TRUE)
-            do.call(plotxs, plotxsobject)
-            assign(x = "plotxsobject", value = plotxsobject, envir = parent.frame())
-        dev.flush()    
+    if (any(vapply(c("Up", "Down", "Left", "Right"), identical, logical(1), key)
+        )){
+        plotxsobject <- get("plotxsobject", envir = parent.frame())
+        Xc <- get("Xc", envir = parent.frame())
+        Xc.cond <- plotxsobject$xc.cond
+        if (plotxsobject$view3d){
+            dev.hold()
+                try(plotxsobject$xc.cond <- get("Xc.cond", envir = parent.frame()), silent = TRUE)
+                if (identical(key, "Down"))
+                    plotxsobject$phi3d <- plotxsobject$phi3d + 2
+                if (identical(key, "Up"))
+                    plotxsobject$phi3d <- plotxsobject$phi3d - 2
+                if (identical(key, "Left"))
+                    plotxsobject$theta3d <- plotxsobject$theta3d + 2
+                if (identical(key, "Right"))
+                    plotxsobject$theta3d <- plotxsobject$theta3d - 2
+                dev.set(expectationwindow)
+                par(bg = "white")
+                close.screen(all.screens = TRUE)
+                do.call(plotxs, plotxsobject)
+                assign(x = "plotxsobject", value = plotxsobject, envir = parent.frame())
+            dev.flush()    
         }    
     }
     if (identical(key, "s")){
