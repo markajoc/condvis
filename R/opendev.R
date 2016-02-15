a286e4c91abe26bce587419706d903b52cb9bfc8
Mark O'Connell <mark_ajoc@yahoo.ie>
2016-02-15 17:23:48 +0000

    add function for opening a device enabled for interactivity

---
+++ R/opendev.R
@@ -0,0 +1,14 @@
+# open a device suitable for interactivity with getGraphicsEvent
+opendev <-
+function (width = 7, height = 7)
+{
+    if (identical(.Platform$OS.type, "windows")){
+        windows(width = width, height = height)
+    } else {
+        if (identical(version$os, "linux-gnu")){
+            x11(type = "Xlib", width = width, height = height)
+        } else {
+            x11(type = "Xlib", width = width, height = height)
+        }
+    }
+}
