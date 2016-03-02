deb59d7b69ea2687ea72d6084d12ca5d3e8f873d
Mark O'Connell <mark_ajoc@yahoo.ie>
2016-03-02 14:02:26 +0000

    fix .trees request as it fails with 'object' being S4

--- R/predict1.R
+++ R/predict1.R
@@ -1,11 +1,14 @@
-predict1 <- 
+predict1 <-
 # predict wrapper to deal with awkward predict methods
 function (object, ...)
 {
     type <- if (inherits(object, "nnet"))
         "class"
-    else if (inherits(object, "rpart")) 
-        "vector"
-    else "response"    
-    predict(object, ..., n.trees = object$n.trees, type = type)
+    else if (inherits(object, "rpart"))
+            "vector"
+        else "response"
+    n.trees <- if (inherits(object, "gbm"))
+        object$n.trees
+    else NULL
+    predict(object, ..., n.trees = n.trees, type = type)
 }
