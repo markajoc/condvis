01ac3b614755ccd628f6f9fe75867cf3db961394
Mark O'Connell <mark_ajoc@yahoo.ie>
2016-03-07 16:44:53 +0000

    attempt to start displaying probabilities on classifier sections instead of just expected class

---
+++ R/glyphs.R
@@ -0,0 +1,30 @@
+myglyph <-
+function (x, y, xw, yw, heights, col)
+{
+  left <- x - 0.5 * xw
+  right <- x + 0.5 * xw
+  top <- y + 0.5 * yw
+  bottom <- y - 0.5 * yw
+  barwidth <- xw / length(heights)
+  barleft <- seq(left, right - barwidth, barwidth)
+  if (any(heights < 0))
+    stop("cannot handle negative 'heights'")
+  if (any(heights > 1))
+    heights <- heights / max(heights)
+  rect(xleft = barleft, xright = barleft + barwidth,
+    ybottom = bottom, ytop = bottom + heights * yw,
+    col = col
+  )
+}
+
+extractprobs <-
+function (model, pred)
+{
+  if (inherits(model, "svm")){
+    if ("probabilities" %in% names(attributes(pred))){
+      p <- attr(pred, "probabilities")
+    } else stop("predictions do not have 'probabilities' attribute,\n ",
+      "maybe svm was fitted without 'probability == TRUE'")
+  }
+  p
+}
