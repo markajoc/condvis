4e676a8bb62393426c04242a3ba3b4b0a505ed81
Mark O'Connell <mark_ajoc@yahoo.ie>
2016-03-10 10:09:54 +0000

    add TODO on slow code in visualweight

--- R/visualweight.R
+++ R/visualweight.R
@@ -3,40 +3,47 @@ function (xc.cond, xc, sigma = NULL, distance = "euclidean", basicoutput =
     FALSE)
 {
     if(!is.data.frame(xc))
-        stop("'xc' should be a data.frame.")
+      stop("'xc' should be a data.frame.")
     if(!is.data.frame(xc.cond) || !(all(names(xc) %in% names(xc.cond))))
-        stop("'xc.cond' must be a data.frame with 1 row,",
-             " and same names as 'xc'.")
+      stop("'xc.cond' must be a data.frame with 1 row,",
+        " and same names as 'xc'.")
     sigma <- if (is.null(sigma))
-        1
+      1
     else sigma
     xc.cond <- xc.cond[, colnames(xc), drop = FALSE]
     if (identical(distance, "daisy")){
-        d <- daisy1(rbind(xc.cond, xc), stand = TRUE)
-        k <- rep(0, nrow(xc))
-        k[d < sigma] <- 0.4
-        k[d < (0.6 * sigma)] <- 0.7
-        k[d < (0.3 * sigma)] <- 1
+      d <- daisy1(rbind(xc.cond, xc), stand = TRUE)
+      k <- rep(0, nrow(xc))
+      k[d < sigma] <- 0.4
+      k[d < (0.6 * sigma)] <- 0.7
+      k[d < (0.3 * sigma)] <- 1
     } else {
-    arefactors <- vapply(xc, is.factor, logical(1))
-    xc.factors <- xc[, arefactors, drop = FALSE]
-    xc.cond.factors <- xc.cond[, arefactors, drop = FALSE]
-    xc.num <- xc[, !arefactors, drop = FALSE]
-    xc.cond.num <- xc.cond[, !arefactors, drop = FALSE]
-    factormatches <- if (any(arefactors)){
+      arefactors <- vapply(xc, is.factor, logical(1))
+      xc.factors <- xc[, arefactors, drop = FALSE]
+      xc.cond.factors <- xc.cond[, arefactors, drop = FALSE]
+      xc.num <- xc[, !arefactors, drop = FALSE]
+      xc.cond.num <- xc.cond[, !arefactors, drop = FALSE]
+
+## TODO: this code for finding cases with matching factor levels is
+## very slow!
+
+      factormatches <- if (any(arefactors)){
         factormatches <- apply(as.matrix(xc.factors), 1,
         function(x) all(x == xc.cond.factors))
-    } else rep(TRUE, nrow(xc))
-    k <- rep(0, nrow(xc))
-    if (all(!factormatches))
+      } else rep(TRUE, nrow(xc))
+
+## end of slow code
+
+      k <- rep(0, nrow(xc))
+      if (all(!factormatches))
         return(list(k = rep(0, nrow(xc)), order = integer(0), sigma = sigma,
-            distance = distance))
-    if (all(arefactors)){
+          distance = distance))
+      if (all(arefactors)){
         k[factormatches] <- 1
         return(list(k = k, order = which(k == 1), sigma = sigma, distance =
-            distance))
-    }
-    if (identical(distance, "euclidean")){
+          distance))
+      }
+      if (identical(distance, "euclidean")){
         x.mean <- colMeans(xc.num)
         x.sd <- apply(xc.num, 2L, sd)
         x.scaled <- scale(xc.num)[factormatches, ]
@@ -45,7 +52,7 @@ function (xc.cond, xc, sigma = NULL, distance = "euclidean", basicoutput =
         k[factormatches][d < (sigma ^ 2)] <- 0.4
         k[factormatches][d < ((0.6 * sigma) ^ 2)] <- 0.7
         k[factormatches][d < ((0.3 * sigma) ^ 2)] <- 1
-    } else if (identical(distance, "maxnorm")){
+      } else if (identical(distance, "maxnorm")){
         x.mean <- colMeans(xc.num)
         x.sd <- apply(xc.num, 2L, sd)
         x.scaled <- scale(xc.num)[factormatches, ]
@@ -54,14 +61,13 @@ function (xc.cond, xc, sigma = NULL, distance = "euclidean", basicoutput =
         k[factormatches][d < sigma] <- 0.4
         k[factormatches][d < (0.6 * sigma)] <- 0.7
         k[factormatches][d < (0.3 * sigma)] <- 1
-    }
+      }
     }
     if (basicoutput)
-        return(k)
+      return(k)
     else {
-        k.order <- order(k)
-        k.order.trimmed <- k.order[k[k.order] > 0]
-        list(k = k, order = k.order.trimmed, sigma = sigma, distance = distance)
+      k.order <- order(k)
+      k.order.trimmed <- k.order[k[k.order] > 0]
+      list(k = k, order = k.order.trimmed, sigma = sigma, distance = distance)
     }
-
 }
