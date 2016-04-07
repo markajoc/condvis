d1d0fc3fdb30daff72ada568efe3055d712923ca
Mark O'Connell <mark_ajoc@yahoo.ie>
2016-04-07 15:56:46 +0100

    faster check for matching categorical elements

--- R/visualweight.R
+++ R/visualweight.R
@@ -28,7 +28,10 @@ function (xc.cond, xc, sigma = NULL, distance = "euclidean", basicoutput =
     rownames(tmp) <- colnames(tmp) <- rownames(tmp.cond) <-
       colnames(tmp.cond) <- NULL
     factormatches <- if (any(arefactors)){
-      apply(tmp, 1, identical, tmp.cond[1, ])
+# potentially faster implementation      
+      apply(tmp == matrix(tmp.cond, ncol = length(tmp.cond), nrow = nrow(tmp),
+        byrow = TRUE), 1L, all)
+#      apply(tmp, 1, identical, tmp.cond[1, ])
     } else rep(TRUE, nrow(xc))
     k <- rep(0, nrow(xc))
     if (all(!factormatches))
