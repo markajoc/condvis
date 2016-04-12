c8ffbee9858df6f6ac0f396793b9a7ae1dac143f
Mark O'Connell <mark_ajoc@yahoo.ie>
2016-04-12 12:38:47 +0100

    fix linebreak

--- R/factordist.R
+++ R/factordist.R
@@ -5,6 +5,6 @@ function(x, X)
   X <- if (is.null(dim(X)))
     matrix(X, ncol = length(x))
   else as.matrix(X)
-  mp <- rowSums(X == matrix(x, ncol = length(x), nrow = nrow(X), byrow = TRUE))
-    / length(x)
+  mp <- rowSums(X == matrix(x, ncol = length(x), nrow = nrow(X), byrow = TRUE)
+    ) / length(x)
 }
