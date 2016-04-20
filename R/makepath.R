25619de6b4eb958025f3f834dd76b2d8d2a2ff93
Mark O'Connell <mark_ajoc@yahoo.ie>
2016-04-20 12:17:38 +0100

    interpolate path for mixed data

--- R/makepath.R
+++ R/makepath.R
@@ -4,13 +4,32 @@ makepath <-
 # Have now added possibility for factors
 function (Xc, ncentroids, ninterp = 4)
 {
-  if (any(apply(Xc, 2L, is.factor))){
+  if (any(arefactors <- vapply(Xc, is.factor, logical(1L)))){
+    if (identical(ninterp %% 2, 0))
+      ninterp <- ninterp + 1
+  }
+  interp <- function(x, n = ninterp)
+  {
+    out <- vector()
+    if (is.factor(x)){
+      for (i in 1:(length(x) - 1L)){
+        out <- c(out, rep(x[i], ceiling(n / 2)), rep(x[i + 1], floor(n / 2)))
+      }
+      return(factor(levels(x)[c(out)], levels = levels(x)))
+    } else {
+      for (i in 1:(length(x) - 1L)){
+        out <- c(out, seq(x[i], x[i + 1L], length.out = n + 1L)[-(n + 1L)])
+      }
+    }
+    out
+  }
+  if (any(arefactors)){
     if (!requireNamespace("cluster", quietly = TRUE))
       stop("requires package 'cluster'")
     d <- cluster::daisy(Xc)
     clustering <- cluster::pam(d, k = ncentroids)
-    centers <- clustering$medoids
-    path <- centers
+    centers <- Xc[clustering$medoids, ]
+    path <- as.data.frame(lapply(centers, interp))
   } else {
     if (!requireNamespace("TSP", quietly = TRUE))
       stop("requires package 'TSP'")
@@ -22,14 +41,6 @@ function (Xc, ncentroids, ninterp = 4)
     o <- TSP::TSP(dist(centers))
     orderindex <- TSP::solve_TSP(o)
     centers <- centers[orderindex, , drop = FALSE]
-    interp <- function(x, n = ninterp)
-    {
-      out <- vector()
-      for (i in 1:(length(x) - 1L)){
-        out <- c(out, seq(x[i], x[i + 1L], length.out = n + 1L)[-(n + 1L)])
-      }
-      out
-    }
     centers <- as.data.frame(t(apply(t(apply(centers, 1L, `*`, sds)), 1L, `+`,
       means)))
     path <- as.data.frame(apply(centers, 2L, interp))
