043140602dc41f4127f88954f75ef32968f26a0f
Mark O'Connell <mark_ajoc@yahoo.ie>
2016-04-22 11:02:18 +0100

    do nrow and ncol once, reflow code

--- R/arrangeC.R
+++ R/arrangeC.R
@@ -1,29 +1,30 @@
 arrangeC <- function (data, method = "default")
 {
-    if (ncol(data) <= 2L)
-        return(list(colnames(data)))
-    data <- na.omit(data)
-    n <- max(36800 - 6850 * log(ncol(data)), 100) # approx n that is practical
-    nr <- nrow(data)
-    if(nr > n)
-      data <- data[sample(1:nr, n, replace = TRUE), ]
-    saving <- matrix(nrow = ncol(data), ncol = ncol(data))
-    colnames(saving) <- rownames(saving) <- colnames(data)
-    for (i in 1:ncol(saving)){
-        for (j in i:ncol(saving)){
-            saving[i, j] <-
-            saving[j, i] <- savingby2d(data[, i], data[, j], method)
-        }
+  nc.data <- ncol(data)
+  if (nc.data <= 2L)
+    return(list(colnames(data)))
+  data <- na.omit(data)
+  nr.data <- nrow(data)
+  n <- max(36800 - 6850 * log(nc.data), 100) # approx n that is practical
+  if(nr.data > n)
+    data <- data[sample(1:nr.data, n, replace = TRUE), ]
+  saving <- matrix(nrow = nc.data, ncol = nc.data)
+  colnames(saving) <- rownames(saving) <- colnames(data)
+  for (i in 1:nc.data){
+    for (j in i:nc.data){
+      saving[i, j] <-
+      saving[j, i] <- savingby2d(data[, i], data[, j], method)
     }
-    diag(saving) <- 1
-    C <- list()
-    i <- 1L
-    while(ncol(saving) > 2){
-        pair <- which(saving == min(saving), arr.ind = TRUE)[1L, ]
-        C[[i]] <- colnames(saving)[pair]
-        saving <- saving[-pair, -pair, drop = FALSE]
-        i <- i + 1L
-        }
-    C[[i]] <- colnames(saving)
-    C
+  }
+  diag(saving) <- 1
+  C <- list()
+  i <- 1L
+  while(ncol(saving) > 2){
+    pair <- which(saving == min(saving), arr.ind = TRUE)[1L, ]
+    C[[i]] <- colnames(saving)[pair]
+    saving <- saving[-pair, -pair, drop = FALSE]
+    i <- i + 1L
+  }
+  C[[i]] <- colnames(saving)
+  C
 }
