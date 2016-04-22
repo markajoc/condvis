2c7f5731fcdd35eae1efa2127484b89310a89956
Mark O'Connell <mark_ajoc@yahoo.ie>
2016-04-22 11:35:01 +0100

    reflow code

--- R/xy2index.R
+++ R/xy2index.R
@@ -3,10 +3,9 @@ xy2index <-
 # soon to be deprecated
 function (x, y, screen.info)
 {
-    c1 <- x >= screen.info$xleft
-    c2 <- x < screen.info$xright
-    c3 <- y >= screen.info$ybottom
-    c4 <- y < screen.info$ytop
-
-    screen.info$xcplots.index[c1 & c2 & c3 & c4]
+  c1 <- x >= screen.info$xleft
+  c2 <- x < screen.info$xright
+  c3 <- y >= screen.info$ybottom
+  c4 <- y < screen.info$ytop
+  screen.info$xcplots.index[c1 & c2 & c3 & c4]
 }
