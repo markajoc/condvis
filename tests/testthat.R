b9a8163843e753a7ffcd9c57e34027843f708f0e
Mark O'Connell <mark_ajoc@yahoo.ie>
2016-05-24 18:10:52 +0100

    add tests directory, begin with visualweight

---
+++ tests/testthat.R
@@ -0,0 +1,3 @@
+library(testthat)
+library(condvis)
+test_check("condvis")
---
+++ tests/testthat/test.visualweight.R
@@ -0,0 +1,35 @@
+context("visualweight")
+
+test_that("visualweight fails without required inputs", {
+  expect_error(visualweight(xc.cond = mtcars, xc = NULL))
+  expect_error(visualweight(xc.cond = NULL, xc = NULL))
+})
+
+test_that("identical observations have visual weight one", {
+  expect_equal(visualweight(mtcars[1, ], mtcars[1, ]), 1)
+  expect_equivalent(diag(visualweight(mtcars, mtcars)), rep(1, nrow(mtcars)))
+  expect_equal(visualweight(mtcars[1:10, ], mtcars[1:5, ]), t(visualweight(
+    mtcars[1:5, ], mtcars[1:10, ])))
+})
+
+test_that("internal visual weight function returns a function", {
+  expect_is(visualweight2(mtcars), "function")
+  expect_error(visualweight2())
+})
+
+n <- 250000
+x1 <- factor(sample(letters[1:3], size = n, replace = TRUE))
+x2 <- factor(sample(letters[4:6], size = n, replace = TRUE))
+x3 <- factor(sample(letters[7:9], size = n, replace = TRUE))
+x4 <- factor(sample(letters[10:15], size = n, replace = TRUE))
+x5 <- rnorm(n)
+x6 <- rnorm(n)
+x7 <- rnorm(n)
+x8 <- rnorm(n)
+
+dat <- data.frame(x1, x2, x3, x4, x5, x6, x7, x8)[, sample(1:8, 50, replace =
+  TRUE)]
+
+test_that("visualweight is not too slow", {
+  takes_less_than(2)(visualweight(xc.cond = dat[1, ], xc = dat))
+})
