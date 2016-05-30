72d23de35c398d589612339f0fdb6127067ae709
Mark O'Connell <mark_ajoc@yahoo.ie>
2016-05-30 14:02:23 +0100

    condense code

--- R/confpred.R
+++ R/confpred.R
@@ -1,40 +1,39 @@
 ## This is a slightly hacky function to extract confidence bounds on predictions
-## from certain classes of model. Most notable is the approach for models of
-## class "custompred", which will be a wrapper object for some model that has no
-## predict method, designed to behave like an lm object under a predict call.
+## from certain classes of model, and pass them on in a standard fashion. Most
+## notable is the approach for models of class "custompred", which will be a
+## wrapper object for some model that has no predict method, designed to behave
+## like an lm object under a predict call.
 
 confpred <-
 function (model, newdata)
 {
-    if (identical(class(model), "lm")){
-        pred <- predict(model, newdata, interval = "confidence", type =
-            "response")
-        upr <- pred[, "upr"]
-        lwr <- pred[, "lwr"]
-        return(cbind(lwr, upr))
+  if (identical(class(model), "lm")){
+    pred <- predict(model = model, newdata = newdata, interval = "confidence",
+      type = "response")
+    upr <- pred[, "upr"]
+    lwr <- pred[, "lwr"]
+    return(cbind(lwr, upr))
+  }
+  if (inherits(model, "custompred")){
+    pred <- predict(model = model, newdata = newdata, interval = "confidence")
+    upr <- pred[, "upr"]
+    lwr <- pred[, "lwr"]
+    return(cbind(lwr, upr))
+  }
+  if (identical(class(model), c("glm", "lm"))){
+    pred <- predict(model = model, newdata = newdata, type = "link", se.fit =
+      TRUE)
+    upr <- model$family$linkinv(pred$fit + (2 * pred$se.fit))
+    lwr <- model$family$linkinv(pred$fit - (2 * pred$se.fit))
+    return(cbind(lwr, upr))
     }
-    if (inherits(model, "custompred")){
-        pred <- predict(model, newdata, interval = "confidence")
-        upr <- pred[, "upr"]
-        lwr <- pred[, "lwr"]
-        return(cbind(lwr, upr))
-    }
-    if (identical(class(model), c("glm", "lm"))){
-        pred <- predict(model, newdata, type = "link", se.fit = TRUE)
-        upr <- pred$fit + (2 * pred$se.fit)
-        lwr <- pred$fit - (2 * pred$se.fit)
-        upr <- model$family$linkinv(upr)
-        lwr <- model$family$linkinv(lwr)
-        return(cbind(lwr, upr))
-    }
-    if (identical(class(model), c("gam", "glm", "lm")) && "mgcv.conv" %in%
-        names(model)){
-        pred <- predict(model, newdata, type = "link", se.fit = TRUE)
-        upr <- pred$fit + (2 * pred$se.fit)
-        lwr <- pred$fit - (2 * pred$se.fit)
-        upr <- model$family$linkinv(upr)
-        lwr <- model$family$linkinv(lwr)
-        return(cbind(lwr, upr))
-    }
-    NULL
+  if (identical(class(model), c("gam", "glm", "lm")) && "mgcv.conv" %in% names(
+    model)){
+    pred <- predict(model = model, newdata = newdata, type = "link", se.fit =
+      TRUE)
+    upr <- model$family$linkinv(pred$fit + (2 * pred$se.fit))
+    lwr <- model$family$linkinv(pred$fit - (2 * pred$se.fit))
+    return(cbind(lwr, upr))
+  }
+  NULL
 }
