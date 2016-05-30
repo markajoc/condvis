4e48e2c8f700419e9bf51153ba2800e99da672bb
Mark O'Connell <mark_ajoc@yahoo.ie>
2016-05-30 17:29:24 +0100

    add example

--- R/cont2color.R
+++ R/cont2color.R
@@ -15,7 +15,14 @@
 #'
 #' @details Uses the \code{RColorBrewer} package if installed.
 #'
+#' @examples
+#' x <- runif(200)
+#' plot(x, col = cont2color(x, c(0,1)))
 #'
+#' plot(x, col = cont2color(x, c(0,0.5)))
+#'
+#' plot(sort(x), col = cont2color(sort(x), c(0.25,0.75)), pch = 16)
+#' abline(h = c(0.25, 0.75), lty = 3)
 
 cont2color <-
 function (x, xrange = NULL, breaks = NULL, colors = NULL)
