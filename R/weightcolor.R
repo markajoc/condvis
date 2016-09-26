abfcdec767195372a437a0cee425228c77ae482a
Mark O'Connell <mark_ajoc@yahoo.ie>
2016-09-26 12:58:24 +0100

    comment new code and NEWS

--- R/weightcolor.R
+++ R/weightcolor.R
@@ -1,14 +1,26 @@
+## Function to weight colours according to a weight vector. Not exported.
+
 weightcolor <-
 function(col, weights)
 {
   n <- length(weights)
   col <- rep(col, n)
+
+  ## We won't perform calculations on elements with `weight` == 0.
+
   weightsgr0 <- which(weights > 0)
   data.order <- weightsgr0[order(weights[weightsgr0])]
+
+  ## Linearly fade the colours in `col` to white in RGB space according to their
+  ## `weights`.
+
   newcol <- (col2rgb(col[data.order]) * matrix(rep(weights[data.order], 3),
     nrow = 3, byrow = TRUE) / 255) + matrix(rep(1 - weights[data.order], 3),
     nrow = 3, byrow = TRUE)
   data.colour <- rep(NA, n)
   data.colour[data.order] <- rgb(t(newcol))
+
+  ## Return the weighted colours with the order as attribute.
+
   structure(data.colour, order = data.order)
 }
