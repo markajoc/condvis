53e856b02490b22240315c883138989233a04574
Mark O'Connell <mark_ajoc@yahoo.ie>
2018-04-17 00:24:40 +0100

    add simple test for interpolate

--- tests/testthat/test.interpolate.R
+++ tests/testthat/test.interpolate.R
@@ -1,9 +1,9 @@
 context("interpolate")
 
 test_that("interpolate throws an error with a negative 'ninterp'", {
-  expect_error(interpolate(1:5, ninterp = -1))  
+  expect_error(interpolate(1:5, ninterp = -1))
 })
 
 test_that("interpolate works on a numeric vector of length 2", {
-  expect_equal(interpolate(c()))
+  expect_equal(interpolate(x = 1:2, ninterp = 3), c(1, 1.25, 1.50, 1.75, 2))
 })
